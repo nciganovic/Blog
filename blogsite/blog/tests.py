9e3935e514c93965469107ddd9b41c805a628dfe
Nikola Ciganović <nciganovic99@gmail.com>
2019-07-29 22:43:29 +0200

    updated test.py

--- blogsite/blog/tests.py
+++ blogsite/blog/tests.py
@@ -1,14 +1,13 @@
 """ Testing and debugging blogsite """
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.http import HttpRequest
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from .models import Blog, Categories
-from . import views
-from .forms import myUserCreationForm 
+from .views import index, register
+from .forms import myUserCreationForm, myAuthenticationForm
 
 
 class BlogTestCase(TestCase):
@@ -44,7 +43,7 @@ class BlogTestCase(TestCase):
         self.assertEqual(c.__str__(), c.category_name)
 
     #--TESTING VIEWS.PY--
-    #Index
+    #INDEX
     def test_blog_views_index(self):
         '''Testing index page at views.py'''
         c = self.create_Categories()
@@ -55,7 +54,7 @@ class BlogTestCase(TestCase):
         resp_decoded = resp.content.decode()
         self.assertIn(c.category_name, resp_decoded)
     
-    #Register
+    #REGISTER
     def test_blog_views_register_get(self):
         
         #Working with get method
@@ -82,21 +81,58 @@ class BlogTestCase(TestCase):
         )
         print('\n Register form errors: ',register_form.errors)
         print('\n Register non field form errors: ',register_form.non_field_errors)
-            
-        url = reverse('register')
-        resp_post = self.client.get(url)
-
         self.assertTrue(register_form.is_valid())
         self.assertEqual(register_form.cleaned_data['username'], "Test_username_new")
         self.assertEqual(register_form.cleaned_data['password1'], "testpassword123")
 
+        '''
+        request = HttpRequest()
+        request.method = 'POST'
+
+        response = register(request)
+
+        url = reverse('register')
+        resp_post = self.client.post(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['location'], '/')
+       
         self.assertRedirects(resp_post, '/index/', status_code=302, target_status_code=200, 
         msg_prefix='', fetch_redirect_response=True)
         '''
-        self.assertEqual(form.first_name, "Test_name_new")
-        self.assertEqual(form.last_name, "Test_name_2")
-        self.assertEqual(form.email, "test@gmail.com")
-        self.assertEqual(form.username, "Test_username_new")
-        self.assertEqual(form.check_password('testpassword123'), True)
+    #LOGIN
+    def test_blog_views_login_get(self): 
+        url = reverse('login')
+        resp_get = self.client.get(url)
+        self.assertEqual(resp_get.status_code, 200)
+
+    def test_blog_views_login_post(self): 
+        url = reverse('login')
+        resp_post = self.client.post(url)
+        self.assertEqual(resp_post.status_code, 200)
+    
+    #TODO fix login test
         '''
-# Create your tests here.
+    @override_settings(LOGIN_URL='/login/')
+    def test_blog_views_login_form(self):
+        response = self.client.get(reverse('/'))
+        self.assertRedirects(response, '/login/?next=/')
+        
+        self.failUnlessEqual(self.client.login(username='Test_username',password='testpassword'),True)
+        print('\n Login form errors: ',login_form.errors)
+        
+        self.assertTrue(login_form.is_valid())   
+        self.assertEqual(login_form.cleaned_data['username'], "Test_username")
+        self.assertEqual(login_form.cleaned_data['password'], "testpassword")
+        '''
+
+    #TODO fix logout test
+    def test_blog_views_logout(self):
+        url = reverse('index')
+        resp = self.client.get(url)
+        resp = resp.content.decode()
+
+        self.client.logout()
+        self.assertIn('Login', resp)
+
+        
