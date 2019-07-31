f48e7165a4092dfb7082db0424db33a2ed814bac
Nikola Ciganović <nciganovic99@gmail.com>
2019-07-31 18:49:42 +0200

    test.py 50%

--- blogsite/blog/tests.py
+++ blogsite/blog/tests.py
@@ -46,16 +46,23 @@ class TestModels(TestCase):
         self.assertEqual(b.__str__(), b.headline)
         self.assertEqual(c.__str__(), c.category_name)
  
-    #INDEX
+class TestViews(TestCase):
+    def setUp(self):
+            self.client = Client()
+            self.Test_Models = TestModels()
+            user = User.objects.create(username='test', email='test@test.com', is_active=True)
+            user.set_password('Test1234')
+            user.save()
+
     def test_views_index(self):
         '''Testing index page at views.py'''
-        c = self.create_Categories()
+        c = self.Test_Models.create_Categories()
         resp = self.client.get(reverse('index'))
         
         self.assertEqual(resp.status_code, 200)
         resp_decoded = resp.content.decode()
         self.assertIn(c.category_name, resp_decoded)
-    
+ 
     #REGISTER
     def test_views_register_GET(self):
         '''Register GET method'''
@@ -67,93 +74,84 @@ class TestModels(TestCase):
         url = reverse('register')
         resp_post = self.client.post(url)
         self.assertEqual(resp_post.status_code, 200)
-        
-    def test_views_register_form(self): 
-        '''
+
+    def test_view_is_valid_register_form(self):
         register_form = myUserCreationForm(data={
             'first_name':"Test_name_new",
             'last_name':"Test_name_2",
             'email':"test@gmail.com",
-            'username':"Test_username_new_1",
+            'username':"Test_username_new",
             'password1': "testpassword123",
             'password2': "testpassword123", }
         )
-        '''
+        #print('\n Register form errors: ',register_form.errors)
+       #print('\n Register non field form errors: ',register_form.non_field_errors)
+        self.assertTrue(register_form.is_valid())
+        self.assertEqual(register_form.cleaned_data['username'], "Test_username_new")
+        self.assertEqual(register_form.cleaned_data['password1'], "testpassword123")
+        self.client.login(username = 'Test_username_new', password="testpassword123")
+
+    def test_views_redirect_register_form(self): 
+        '''testing redirecting form'''
         url = reverse('register')
         resp_post = self.client.post(url, {
             'first_name':"Test_name_new",
             'last_name':"Test_name_2",
             'email':"test@gmail.com",
-            'username':"Test_username_new_1",
+            'username':"Test_username_new",
             'password1': "testpassword123",
             'password2': "testpassword123", })
 
-        #print('\n Register form errors: ',register_form.errors)
-        #print('\n Register non field form errors: ',register_form.non_field_errors)
-        #self.assertTrue(register_form.is_valid())
-        #self.assertEqual(register_form.cleaned_data['username'], "Test_username_new")
-        #self.assertEqual(register_form.cleaned_data['password1'], "testpassword123")
-        #self.client.login(username = 'Test_username_new', password="testpassword123")
-        
-
-        self.assertEqual(resp_post.status_code, 302) #NEW SHIT
+        self.assertEqual(resp_post.status_code, 302) 
         self.assertRedirects(resp_post, '/', status_code=302, target_status_code=200, 
         msg_prefix='', fetch_redirect_response=True)
 
-        #TODO fix register test
-        '''
-        request = HttpRequest()
-        request.method = 'POST'
-
-        response = register(request)
-
-        url = reverse('register')
-        resp_post = self.client.post(url)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response['location'], '/')
-       
-        self.assertRedirects(resp_post, '/index/', status_code=302, target_status_code=200, 
-        msg_prefix='', fetch_redirect_response=True)
-        '''
     #LOGIN
-    def test_views_login_get(self): 
+    def test_views_login_GET(self): 
         url = reverse('login')
         resp_get = self.client.get(url)
         self.assertEqual(resp_get.status_code, 200)
 
-    def test_views_login_post(self): 
+    def test_views_login_POST(self): 
         url = reverse('login')
         resp_post = self.client.post(url)
         self.assertEqual(resp_post.status_code, 200)
-    
-    #TODO fix login test
-        '''
-    @override_settings(LOGIN_URL='/login/')
-    def test_blog_views_login_form(self):
-        self.failUnlessEqual(self.client.login(username='lilciga',password='pbkdf2_sha256'),True)
 
-        response = self.client.get(reverse('/'))
-        self.assertRedirects(response, '/login/?next=/')
+    def test_view_is_valid_login_form(self):
+        login_form = myAuthenticationForm(data={
+            'username':"test",
+            'password': "Test1234",
+             }
+        )
+        print('\n login_form errors: ',login_form.errors)
+        print('\n login_form non field errors: ',login_form.non_field_errors)
+        self.assertTrue(login_form.is_valid())
+        self.assertEqual(login_form.cleaned_data['username'], "test")
+        self.assertEqual(login_form.cleaned_data['password'], "Test1234")
+        self.client.login(username='Test_username_new', password="testpassword123")
 
-        print('\n Login form errors: ',login_form.errors)
-        self.assertTrue(login_form.is_valid())   
-        self.assertEqual(login_form.cleaned_data['username'], "Test_username")
-        self.assertEqual(login_form.cleaned_data['password'], "testpassword")
-        '''
+    def test_views_redirect_login(self): 
+        url = reverse('login')
+        resp_post = self.client.post(url, {
+            'username':"test",
+            'password': "Test1234",})
 
+        self.assertEqual(resp_post.status_code, 302) 
+        self.assertRedirects(resp_post, '/', status_code=302, target_status_code=200, 
+        msg_prefix='', fetch_redirect_response=True)
+
+    #LOGOUT  
     #TODO fix logout test
     def test_views_logout(self):
-        url = reverse('index')
-        resp = self.client.get(url)
-        resp = resp.content.decode()
-
-        self.client.logout()
-        self.assertIn('Login', resp)
+        url = reverse('logout')
+        resp_post = self.client.post(url)
+        self.assertEqual(resp_post.status_code, 302)
+        self.assertRedirects(resp_post, '/', status_code=302, target_status_code=200, 
+        msg_prefix='', fetch_redirect_response=True) 
 
     def test_single_slug(self):
         #b = self.create_Blog()
-        c = self.create_Categories()
+        c = self.Test_Models.create_Categories()
         single_slug = 'gaming'
         self.assertEqual(single_slug, c.category_slug)
 
